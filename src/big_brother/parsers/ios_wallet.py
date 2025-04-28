fb6b31c7c621fa7d8940584cc394fb963137ab01
Waqas Ali <waqas.abbasi@outlook.com>
2025-04-28 22:47:54 +0800

    add exception for american express in iOS wallet

--- src/big_brother/parsers/ios_wallet.py
+++ src/big_brother/parsers/ios_wallet.py
@@ -7,6 +7,13 @@ from big_brother.parsers.common import (
 from big_brother.services.mail import EmailMessage
 
 
+def get_account_name(account: str) -> str:
+    card_name = account.replace("Card", "").strip()
+    if "American Express" in card_name:
+        return "American Express"
+    return card_name
+
+
 def parse_ios_wallet_email(email: EmailMessage) -> Transaction:
     regex = f".*You spent ({currency_regex})(.*) at (.*) using (.*) card.*"
     match = re.match(regex, email.body, re.DOTALL)
@@ -18,7 +25,7 @@ def parse_ios_wallet_email(email: EmailMessage) -> Transaction:
     return Transaction(
         date=email.date.date(),
         payee=payee,
-        account=account.replace("Card", "").strip(),
+        account=get_account_name(account),
         amount=converted,
         memo=f"Original: {currency} {amount}" if converted != amount else None,
     )
